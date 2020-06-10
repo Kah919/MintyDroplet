5b6231ea993de2a14e751ee51ac27723a2423c32
Kah919 <kahyap13@gmail.com>
2020-06-10 12:31:59 -0400

    First commit

---
+++ src/components/Board.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import anime from 'animejs/lib/anime.es.js';
+import Player from './Player';
+
+export default () => {
+    return(
+        <div className="board">
+            <Player />
+        </div>
+    )
+}
